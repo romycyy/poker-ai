9ce05a69bed6b8ae33a114357f6025fb33501649
chenyiyu <cyy20041234@gmail.com>
2025-03-13 01:16:15 -0400

    changed game tree

---
+++ pluribus.py
@@ -0,0 +1,1 @@
+# sunday fill in CFR
--- utils/poker_tree.py
+++ utils/poker_tree.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Any
+from typing import List, Union, Any, Callable
 from enum import Enum
 import random
 
@@ -39,14 +39,23 @@ class Card:
     def __repr__(self):
         return f"{self.rank.value}{self.suit.value}"
 
+class History:
+    def __init__(self,
+        actions: List[Action],
+        cards: List[Card]):
+        self.actions = actions
+        self.cards = cards
+
 class Player:
     def __init__(self,
         dealer: bool = False,
-        stack: int = 0):
+        stack: int = 0,
+        strategy: Callable = None):
         self.stack: int = stack
         self.cards: List[Card] = None
         self.pos: int = 0
         self.dealer: bool = dealer
+        self.strategy: Callable[[History], Action] = strategy
     
     def set_cards(self, cards: List[Card]):
         self.cards = cards
@@ -59,13 +68,16 @@ class Player:
 
     def get_dealer(self):
         return self.dealer
-
+    
+    def get_actions(self, history: History):
+        return self.strategy(history)
+    
 class ActionType(Enum):
     FOLD = "fold"
     CALL = "call"
     RAISE = "raise"
     CHECK = "check"
-    START = "start"
+    PRE_FLOP = "pre_flop"
     FLOP = "flop"
     TURN = "turn"
     RIVER = "river"
@@ -83,7 +95,6 @@ class Action:
 class GameNode:
     """
     Represents a node (history) h in the game tree.
-    - history: all relevant info to define the state (public cards, private cards, bets so far, etc.)
     - player_to_act: which player is acting now, or 'chance' if it's a chance node
     - actions: list of possible actions (A(h))
     - is_terminal: True if this is a terminal node in the game (h ∈ Z)
@@ -131,31 +142,30 @@ class GameTree:
         self.players: List[Player] = players
         self.dealer_pos: int = dealer_pos
         self.current_stage: ActionType = None
+        self.current_pos: int = 0
         self.dealer: Player = None
         self.nodes: List[GameNode] = []
         self.deck: List[Card] = [Card(rank, suit) for rank in Rank for suit in Suit]
 
     def start_game(self):
         assert self.current_stage == None
-        dealer = Player(dealer=True)
-        self.dealer = dealer
-        self.nodes.append(GameNode(player_to_act=dealer, actions=[Action(ActionType.START, 0)], is_terminal=False))
-        self.current_stage = ActionType.START
+        self.dealer = Player(dealer=True)
+        self.nodes.append(GameNode(player_to_act=self.dealer, actions=[Action(ActionType.PRE_FLOP, 0)], is_terminal=False))
+        self.current_stage = ActionType.PRE_FLOP
         random.shuffle(self.deck)
         for i in range(len(self.players)):
             self.players[i].set_pos((i-self.dealer_pos)%len(self.players))
             # Deal 2 cards to each player
             self.players[i].set_cards([self.deck.pop(), self.deck.pop()])
 
-
     def get_root(self):
-        return self.root
+        return self.nodes[0]
     
     def get_node(self, node_id: str):
         return self.nodes[node_id]
     
-    def get_action_from_player(self, current_node: GameNode) -> Action:
-        player = current_node.get_player_to_act()
+    def get_action_from_player(self, player: Player) -> Action:
+        
 
     
     def next_node(self, current_node: GameNode, action) -> None:
@@ -167,26 +177,4 @@ class GameTree:
         - cards if it's a chance node
         - check if terminal, etc.
         """
-        new_history = current_node.history + [action]  # simplistic: just append action to 'history'
-        new_actions = []
 
-        # 1. If chance node, generate possible deals or outcomes
-        if current_node.player_to_act == "chance":
-            # For example, if we are dealing a card from a deck:
-            new_actions = []  # depends on the deck, next deals, etc.
-            next_player = 0   # after chance, player 0 might act
-        else:
-            # 2. If player node, define next player's turn
-            next_player = (current_node.player_to_act + 1) % 6  # example for 6-handed
-            new_actions = ["fold", "call", "raise"]  # simplistic placeholder
-
-        # 3. Check if terminal
-        # For instance, if everyone but one player folded, or if all betting rounds are done
-        is_terminal = check_if_terminal(new_history)
-
-        self.nodes[new_history] = GameNode(history=new_history,
-        # Create and return the new node
-        return GameNode(history=new_history,
-                        player_to_act=next_player,
-                        actions=new_actions,
-                        is_terminal=is_terminal)
